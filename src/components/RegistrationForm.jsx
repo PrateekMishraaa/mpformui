40889b779ac568ddf06f5cea56a33d27f2d467d5
Prateek Mishra <pm921670@gmail.com>
2025-05-20 14:58:01 +0530

    fix

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -39,7 +39,7 @@ const GetStartedForm = () => {
 
     try {
       const response = await axios.post(
-        "https://mpformbackend.onrender.com",
+        "https://mpformbackend.onrender.com/api/contact",
         formData,
         {
           headers: { "Content-Type": "application/json" },
